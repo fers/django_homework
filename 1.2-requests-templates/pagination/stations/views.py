203644a7a529ce5a219d2e797996f2ffb81f779d
Евгений <fersf@ya.ru>
2022-04-17 11:41:08 +0300

    Задание 1.2. Пагинация
    Считывваем номер страницы через параметр страницы
    Открываем CSV файл на чтение
    Читаем CSV файл
    Вычитываем данные в список
    Используем класс пагинатор для формирования данных постранично и передачи через context

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,6 @@
+import csv
+
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -10,8 +13,22 @@ def bus_stations(request):
     # получите текущую страницу и передайте ее в контекст
     # также передайте в контекст список станций на странице
 
+    page_number = int(request.GET.get('page', 1))
+
+    bus_station_list = []
+
+    with open('data-398-2018-08-30.csv', 'r', encoding='UTF-8') as csvfile:
+        csv_reader = csv.DictReader(csvfile)
+
+        for x in csv_reader:
+            bus_station_list.append({'Name': x['Name'], 'Street': x['Street'], 'District': x['District']})
+
+    paginator = Paginator(bus_station_list, 10)
+
+    page = paginator.get_page(page_number)
+
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+         'bus_stations': paginator.get_page(page_number),
+         'page': page
     }
     return render(request, 'stations/index.html', context)
